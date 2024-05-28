a1641802d2c2fd5277a448c48b85d52dac45506c
YourAnthem <163949371+YourAnthem@users.noreply.github.com>
2024-05-28 09:56:12 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCzBk3_R_d_u6nJo5MgBEgYBqkVAeWED9A",
+  authDomain: "crud-f688c.firebaseapp.com",
+  databaseURL: "https://crud-f688c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-f688c",
+  storageBucket: "crud-f688c.appspot.com",
+  messagingSenderId: "729499769552",
+  appId: "1:729499769552:web:b9b7cddbfd1b84aee4dcd5",
+  measurementId: "G-5D09XFPNQ5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
